5e41d85d39ccdcf099ae7006b626fa9640bb229a
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-11-03 19:29:00 -0700

    se agrego el entity al

--- src/categories/entities/category.entity.ts
+++ src/categories/entities/category.entity.ts
@@ -1,7 +1,9 @@
 import { Entity, Column, PrimaryGeneratedColumn, BaseEntity } from 'typeorm';
+@Entity()
 export class Category extends BaseEntity{
   @PrimaryGeneratedColumn()
   id: number;
+
   @Column({type:'varchar', length: 50})
   name: string;
 }
