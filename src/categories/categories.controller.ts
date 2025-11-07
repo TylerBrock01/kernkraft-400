620553a1ac6ba8d24a910adf1854583324a5bd3e
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-11-07 10:45:32 -0700

    Updated added

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -36,6 +36,8 @@ export class CategoriesController {
     @Param('id',IdValidationPipe) id: string,
     @Body() updateCategoryDto: UpdateCategoryDto,
   ) {
+    console.log(id);
+    console.log(updateCategoryDto);
     return this.categoriesService.update(+id, updateCategoryDto);
   }
 
