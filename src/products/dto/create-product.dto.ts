6e4afc2f80e20c4737cba9a845f407486f669692
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-11-18 11:03:34 -0700

    product dto added

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,10 +1,16 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsInt, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateProductDto {
   @(IsNotEmpty({ message: 'nombre es requerido'}))
+  @(IsString({ message: 'nombre debe ser string'}))
   name: string;
-
+  @(IsNotEmpty({ message: 'precio es requerido'}))
+  @(IsNumber({maxDecimalPlaces: 2}, { message: 'precio debe ser un numero'}))
   price: number;
+  @(IsNotEmpty({ message: 'stock es requerido'}))
+  @(IsNumber({maxDecimalPlaces: 0}, { message: 'stock debe ser un numero'}))
   stock: number;
+  @(IsNotEmpty({ message: 'categoria es requerido'}))
+  @(IsInt( { message: 'categoria debe ser un numero'}))
   categoryId: number;
 }
