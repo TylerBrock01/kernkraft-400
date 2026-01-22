5905aa1898062a65dba5e90c7912f4183d6b5b93
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-01-21 21:17:25 -0800

    dev: findOne updated

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -34,8 +34,12 @@ export class ProductsService {
     return {products, total};
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: number) {
+    const product = await this.productRepository.findOne({where: {id}, relations: {category: true}});
+    if (!product) throw new NotFoundException(
+      `Product #${id} not found`
+    )
+    return product;
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
