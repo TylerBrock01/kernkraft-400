57f0ff0fbd66bf8abe338432a40c846973746169
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-01-21 21:56:45 -0800

    dev: remove added filters

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -61,7 +61,10 @@ export class ProductsService {
     return await this.productRepository.save(product);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: number) {
+    const product = await this.findOne(id);
+    if (!product) throw new NotFoundException('Product not found')
+    await this.productRepository.remove(product);
+    return `Product #${id} REMOVED`;
   }
 }
