18f4d6ea39ed48654d17a46493509258456de735
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-01-23 15:48:35 -0800

    dev (trans): transaction find all sales

--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -3,7 +3,7 @@ import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Transaction, TransactionContent } from './entities/transaction.entity';
-import { Repository } from 'typeorm';
+import { FindManyOptions, Repository } from 'typeorm';
 import { Product } from '../products/entities/product.entity';
 
 @Injectable()
@@ -52,7 +52,8 @@ export class TransactionsService {
   }
 
   findAll() {
-    return `This action returns all transactions`;
+    const options : FindManyOptions<Transaction> = {relations: {contents:true}}
+    return this.transactionRepository.find(options);
   }
 
   findOne(id: number) {
