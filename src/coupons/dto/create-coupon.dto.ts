f739b1294cb632d24d1a888d040c2d8a19b51134
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-01-24 12:42:20 -0800

    dev (coupon): dto added

--- src/coupons/dto/create-coupon.dto.ts
+++ src/coupons/dto/create-coupon.dto.ts
@@ -1,1 +1,17 @@
-export class CreateCouponDto {}
+import { IsDateString, IsInt, IsNotEmpty, Max, Min } from 'class-validator';
+
+export class CreateCouponDto {
+
+  @IsNotEmpty({ message: 'nombre es requerido' })
+  name: string;
+
+  @IsNotEmpty({ message: 'descuento es requerido' })
+  @IsInt({ message: 'descuento debe ser un numero'})
+  @Max(100, { message: 'el descuento debe ser menor a 100' })
+  @Min(1, { message: 'el descuento debe ser mayor a 1' })
+  discount: number;
+
+  @IsNotEmpty({ message: 'fecha de expiracion es requerido' })
+  @IsDateString({}, { message: 'fecha de expiracion debe ser una fecha valida' })
+  expirationDate: Date;
+}
