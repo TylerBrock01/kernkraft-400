4eaefd3b8afff5f517a9af0216a717f6174a6295
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-02-04 12:08:43 -0800

    dev (upload-image): upload image function added

--- src/upload-image/upload-image.service.ts
+++ src/upload-image/upload-image.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import {v2 as cloud} from 'cloudinary'
+import {CloudinaryResponse} from './upload-image.response';
+import streamifier from 'streamifier'
 
 @Injectable()
-export class UploadImageService {}
+export class UploadImageService {
+  uploadFile(file : Express.Multer.File): Promise<CloudinaryResponse>{
+    return new Promise<CloudinaryResponse>((resolve, reject) => {
+      const uploadStream = cloud.uploader.upload_stream(
+        (error, result) => {
+          if (error) return reject(error);
+          resolve(result);
+        }
+        )
+      streamifier.createReadStream(file.buffer).pipe(uploadStream)
+    })
+  }
+}
