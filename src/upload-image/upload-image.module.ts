6b3338c53b7552e3fcdc03c74050a932cc24fbaa
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-02-04 11:31:57 -0800

    dev (cloudinary): credential added

--- src/upload-image/upload-image.module.ts
+++ src/upload-image/upload-image.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UploadImageService } from './upload-image.service';
-import { UploadImage } from './upload-image';
+import { UploadImageProvider } from './upload-image';
 
 @Module({
-  providers: [UploadImageService, UploadImage]
+  providers: [UploadImageService, UploadImageProvider]
 })
 export class UploadImageModule {}
