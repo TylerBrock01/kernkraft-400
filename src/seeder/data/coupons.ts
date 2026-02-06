a358cbfe5156170ef8c34d33e60d158d5fc560bd
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-02-06 13:36:34 -0800

    dev(seeder): coupons extended

--- src/seeder/data/coupons.ts
+++ src/seeder/data/coupons.ts
@@ -1,12 +1,54 @@
 export const coupons = [
-  {
-    "name":"Monster",
-    "discount": 40,
-    "expirationDate": "2026-10-25"
-  },
-  {
-    "name":"Redbull",
-    "discount": 30,
-    "expirationDate": "2026-10-25"
-  }
+  [
+    {
+      "name": "MONSTER",
+      "discount": 40,
+      "expirationDate": "2026-12-31"
+    },
+    {
+      "name": "REDBULL",
+      "discount": 30,
+      "expirationDate": "2026-12-31"
+    },
+    {
+      "name": "CAZA10",
+      "discount": 10,
+      "expirationDate": "2026-10-25"
+    },
+    {
+      "name": "SKATEORDIE",
+      "discount": 50,
+      "expirationDate": "2026-12-31"
+    },
+    {
+      "name": "GRAFFITI",
+      "discount": 15,
+      "expirationDate": "2026-08-15"
+    },
+    {
+      "name": "FLIP50",
+      "discount": 50,
+      "expirationDate": "2026-06-30"
+    },
+    {
+      "name": "PENNYLOVE",
+      "discount": 20,
+      "expirationDate": "2026-11-20"
+    },
+    {
+      "name": "ESTUDIANTE",
+      "discount": 25,
+      "expirationDate": "2026-12-15"
+    },
+    {
+      "name": "BEARGRIP",
+      "discount": 10,
+      "expirationDate": "2026-09-01"
+    },
+    {
+      "name": "OLLIE2026",
+      "discount": 20,
+      "expirationDate": "2026-01-01"
+    }
+  ]
 ]
