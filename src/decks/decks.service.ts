7ab2cde711d822f512e86ad3cf048012df8b208e
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-02-05 21:04:07 -0800

    dev(decks) : typeORM create added

--- src/decks/decks.service.ts
+++ src/decks/decks.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreateDeckDto } from './dto/create-deck.dto';
 import { UpdateDeckDto } from './dto/update-deck.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Deck } from './entities/deck.entity';
 
 @Injectable()
 export class DecksService {
+  constructor(
+    @InjectRepository(Deck)
+    private readonly deckRepository: Repository<Deck>,
+  ) {
+  }
   create(createDeckDto: CreateDeckDto) {
-    return 'This action adds a new deck';
+    const deck = new Deck()
+    deck.name = createDeckDto.name;
+    return this.deckRepository.save(deck);
   }
 
   findAll() {
--- src/decks/dto/create-deck.dto.ts
+++ src/decks/dto/create-deck.dto.ts
@@ -1,1 +1,7 @@
-export class CreateDeckDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateDeckDto {
+  @IsString()
+  @IsNotEmpty({message: 'tipo de tabla es requerido'})
+  name: string;
+}
