4fbbdfdc83db833b0272326eaa69ba516ea13b13
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-02-05 21:03:55 -0800

    dev(decks) : added entity typeORM

--- src/decks/entities/deck.entity.ts
+++ src/decks/entities/deck.entity.ts
@@ -1,1 +1,12 @@
-export class Deck {}
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Product } from '../../products/entities/product.entity';
+
+@Entity()
+export class Deck {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column({type: 'varchar', length: 20})
+  name: string;
+  @OneToMany(() => Product, (product) => product.category , {cascade: true} )
+  products: Product[];
+}
