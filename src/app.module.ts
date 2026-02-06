cfe4c9dd2a6ca45706b76941f0004e2371e20d09
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-02-05 20:40:17 -0800

    dev(decks) : deck added

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { ProductsModule } from './products/products.module';
 import { TransactionsModule } from './transactions/transactions.module';
 import { CouponsModule } from './coupons/coupons.module';
 import { UploadImageModule } from './upload-image/upload-image.module';
+import { DecksModule } from './decks/decks.module';
 
 @Module({
   imports: [
@@ -24,6 +25,7 @@ import { UploadImageModule } from './upload-image/upload-image.module';
     TransactionsModule,
     CouponsModule,
     UploadImageModule,
+    DecksModule,
   ],
   controllers: [AppController],
   providers: [AppService],
