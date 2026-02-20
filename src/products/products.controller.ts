6af051bdd2f26ecb724585a64a0dbb71558aca6c
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-02-20 15:30:29 -0800

    guard added

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -9,7 +9,7 @@ import {
   Query,
   Put,
   UseInterceptors,
-  UploadedFile, HttpException, BadRequestException,
+  UploadedFile, HttpException, BadRequestException, UseGuards,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
@@ -18,6 +18,7 @@ import { GetProductQueryDto } from './dto/get-product.dto';
 import { IdValidationPipe } from '../common/pipes/id-validation/id-validation.pipe';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { UploadImageService } from '../upload-image/upload-image.service';
+import { JwtAuthGuard } from '../jwt-auth/jwt-auth.guard';
 
 @Controller('products')
 export class ProductsController {
@@ -26,7 +27,7 @@ export class ProductsController {
     private readonly uploadImageService: UploadImageService,
 
   ) {}
-
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
     console.log(createProductDto);
@@ -47,16 +48,19 @@ export class ProductsController {
     return this.productsService.findOne(+id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   update(@Param('id', IdValidationPipe) id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(+id, updateProductDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   remove(@Param('id', IdValidationPipe) id: string) {
     return this.productsService.remove(+id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post('upload-image')
   @UseInterceptors(FileInterceptor('file'))
   uploadImage(@UploadedFile() file: Express.Multer.File) {
