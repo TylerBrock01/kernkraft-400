2e093915acbea0f03104cd703be439a155394b79
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-04-02 18:21:07 -0700

    cleaned code to remove unused

--- src/audit-logs/audit-logs.controller.ts
+++ src/audit-logs/audit-logs.controller.ts
@@ -7,28 +7,4 @@ import { UpdateAuditLogDto } from './dto/update-audit-log.dto';
 export class AuditLogsController {
   constructor(private readonly auditLogsService: AuditLogsService) {}
 
-  @Post()
-  create(@Body() createAuditLogDto: CreateAuditLogDto) {
-    return this.auditLogsService.create(createAuditLogDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.auditLogsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.auditLogsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuditLogDto: UpdateAuditLogDto) {
-    return this.auditLogsService.update(+id, updateAuditLogDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.auditLogsService.remove(+id);
-  }
 }
