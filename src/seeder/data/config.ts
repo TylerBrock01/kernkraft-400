835edd589504c12be7f977ec48dcae124637cfbd
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-04-03 02:48:42 -0700

    config production added

--- src/seeder/data/config.ts
+++ src/seeder/data/config.ts
@@ -1,5 +1,5 @@
-export const BUSINESS_A_ID = "d80bad8b-3f51-4ca6-bb7a-72dcd9d14c02"; // Empresa A: Tecnología
-export const BUSINESS_B_ID = "73a8c36e-f49a-46e2-86a6-78eca22baaf0"; // Empresa B: Eventos y Tianguis
+export const BUSINESS_A_ID = "8d8e6981-8bdf-48e6-8532-b4775d540256"; // Empresa A: Tecnología
+export const BUSINESS_B_ID = "64029116-0ee2-4de3-a326-ebbc921a6c09"; // Empresa B: Eventos y Tianguis
 
 export const USER_A_ID = 1; // ID numérico del Admin/Vendedor A
 export const USER_B_ID = 2; // ID numérico del Admin/Vendedor B
