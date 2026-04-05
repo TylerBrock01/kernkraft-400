f558df3e12646fc6bd7e5e300dc7f683d80da09a
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-04-04 21:21:12 -0700

    security for production

--- src/business/business.controller.ts
+++ src/business/business.controller.ts
@@ -14,7 +14,7 @@ export class BusinessController {
 
   // 1. REGISTRO: Público o controlado (tú decides si cualquiera puede crear un negocio)
   @Post()
-  @Roles(Role.ADMIN, Role.SUPER_ADMIN)
+  @Roles(Role.SUPER_ADMIN)
   create(@Body() createBusinessDto: CreateBusinessDto) {
     return this.businessService.create(createBusinessDto);
   }
