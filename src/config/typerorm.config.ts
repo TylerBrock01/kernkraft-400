e9d947be61e81d351041d003d66fddc28efdfb72
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-04-13 23:58:50 -0700

    added railway config for database connection

--- src/config/typerorm.config.ts
+++ src/config/typerorm.config.ts
@@ -6,12 +6,13 @@ export const typeOrmConfig = (
   configService: ConfigService,
 ): TypeOrmModuleOptions => ({
   type: 'postgres',
-  host: configService.get<string>('DATABASE_URL'),
-  port: configService.get<number>('DATABASE_PORT'),
-  username: configService.get<string>('DATABASE_USER'),
-  password: configService.get<string>('DATABASE_PASS'),
-  database: configService.get<string>('DATABASE_NAME'),
-  ssl: true,
+  url: configService.get<string>('DATABASE_URL'),//_URL para prod
+  // host: configService.get<string>('DATABASE_HOST'),//_host local para prod
+  // port: configService.get<number>('DATABASE_PORT'),
+  // username: configService.get<string>('DATABASE_USER'),
+  // password: configService.get<string>('DATABASE_PASS'),
+  // database: configService.get<string>('DATABASE_NAME'),
+  ssl: true,//true para produ
   logging: false,
   entities: [join(__dirname, '../**/*.entity.{js,ts}')],
   synchronize: configService.get<string>('NODE_ENV') !== 'production',
