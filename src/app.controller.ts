3e3883e2aa627973273b1249377c9d3aca247ee5
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-04-13 23:19:58 -0700

    added cronjob

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('/health')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  check() {
+    // Un JSON ligerísimo que Render procesa en 2 milisegundos
+    return {
+      status: 'Motor CAZA Operativo 🚀',
+      timestamp: new Date().toISOString(),
+      uptime: process.uptime()
+    };
   }
 }
